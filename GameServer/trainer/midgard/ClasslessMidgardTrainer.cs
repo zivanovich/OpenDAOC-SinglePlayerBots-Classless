6c9ef776e06cf07271e8a3067cfa2cbcbd545999
zivanovich <ivanovich@gmail.com>
2026-09-03 23:05:41 -0500

    ClasslessMidgard trainer added

---
+++ GameServer/trainer/midgard/ClasslessMidgardTrainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using DOL.Language;
+
+namespace DOL.GS.Trainer
+{
+    [NPCGuildScript("Classless Midgard Trainer", eRealm.Midgard)]		// this attribute instructs DOL to use this script for all "Classless Midgard Trainer" NPC's in Midgard (multiple guilds are possible for one script)
+    public class ClasslessMidgardTrainer : GameTrainer
+    {
+        public override eCharacterClass TrainedClass
+        {
+            get { return eCharacterClass.ClasslessMidgard; }
+        }
+
+        public ClasslessMidgardTrainer() : base(eChampionTrainerType.None)
+        {
+        }
+    }
+}
